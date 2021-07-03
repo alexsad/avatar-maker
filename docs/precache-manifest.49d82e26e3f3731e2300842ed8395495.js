101bb80c0d9eb92af9b4ebda303f83a459572aac
alexsad <alex.query@gmail.com>
2021-07-03 15:42:51 -0300

    fix: url site

---
+++ docs/precache-manifest.49d82e26e3f3731e2300842ed8395495.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3d432087be9093dae3",
+    "url": "/avatar-maker/css/app.51043612.css"
+  },
+  {
+    "revision": "e3a79b0d0924d34522675f5196fad777",
+    "url": "/avatar-maker/img/gift-box-white.e3a79b0d.svg"
+  },
+  {
+    "revision": "62e1247e957fd954d7a3c27478d1f55a",
+    "url": "/avatar-maker/img/guia-s21.62e1247e.png"
+  },
+  {
+    "revision": "9ca63e5823115d9dccaf63a836c082ef",
+    "url": "/avatar-maker/index.html"
+  },
+  {
+    "revision": "9a3d432087be9093dae3",
+    "url": "/avatar-maker/js/app.056297ce.js"
+  },
+  {
+    "revision": "d9902b82269038ffb589",
+    "url": "/avatar-maker/js/chunk-2d0d36c6.c7afc97f.js"
+  },
+  {
+    "revision": "876818c8465135a8eadb",
+    "url": "/avatar-maker/js/chunk-2d207e6e.3049b49e.js"
+  },
+  {
+    "revision": "dd5c048f84960515467f",
+    "url": "/avatar-maker/js/chunk-2d230e25.5876f30b.js"
+  },
+  {
+    "revision": "ca55f3bcb2dfbedc59c1",
+    "url": "/avatar-maker/js/chunk-vendors.e46ecbe6.js"
+  },
+  {
+    "revision": "118635a0dd779af31361944c0dc03620",
+    "url": "/avatar-maker/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/avatar-maker/robots.txt"
+  }
+]);
